333151c1b32601589384bc09941d46b6b962328d
Collector0203 <0203wxy@gmail.com>
2026-08-06 14:37:51 +0800

    Add files via upload

---
+++ 1. Tests of convergence rates/2. initial data_algebraic decay/equation1D_initial_alge/s=0.3_p=1_gamma=1/spatial error test/MTcoeff_origin.m
@@ -0,0 +1,16 @@
+% This program uses fft to compute the MT-coefficients {a(k)}_{k=-N:N-1} of
+% 1D function u(x), where a(k) := int(x=-inf:inf) u(x)*conj(MT(k,x)).
+% 
+% input: the function value sequence of u(x) on MT-nodes "U", truncation
+% "N".
+% output: MT-coefficients of u(x) (colunm vector).
+%
+% (remark): When "U" is computed based on the scaled MT-nodes, "coeff =
+% MTcoeff_origin(U,N)" implies that "u(x) \approx sum(j=-N:N-1)
+% coeff(j)*MT(j,mu*x)".
+
+function coeff = MTcoeff_origin(U,N)
+theta = -pi + pi/N*(0:2*N-1).'; % generte theta sequence
+U1 = U.*(1-1i*tan(theta/2));
+U2 = fftsum(U1,N);
+coeff = (-1i).^(-N:N-1).'/2/N*sqrt(pi/2).*U2;
