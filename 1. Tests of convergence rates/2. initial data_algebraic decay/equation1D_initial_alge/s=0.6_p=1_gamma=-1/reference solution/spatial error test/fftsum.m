79ab7c4d9dbdaed445cb37c901f70e0348110996
Collector0203 <0203wxy@gmail.com>
2026-08-06 14:40:25 +0800

    Delete 1. Tests of convergence rates/2. initial data_algebraic decay/equation1D_initial_alge/s=0.6_p=1_gamma=-1/reference solution/spatial error test directory

--- 1. Tests of convergence rates/2. initial data_algebraic decay/equation1D_initial_alge/s=0.6_p=1_gamma=-1/reference solution/spatial error test/fftsum.m
+++
@@ -1,8 +0,0 @@
-% This program uses fft to compute v(k) = sum(j=0:2*N-1)
-% u(j)*exp[-1i*k*theta(j)], theta(j) = -pi+pi/N*j, k = -N:N-1. (This
-% program requires u to be a column vector.)
-
-function v2 = fftsum(u,N)
-v = fft(u);
-vv = [v(N+1:2*N);v(1:N)];
-v2 = (-1).^((0:2*N-1).'+mod(N,2)).*vv;
--- 1. Tests of convergence rates/2. initial data_algebraic decay/equation1D_initial_alge/s=0.6_p=1_gamma=-1/reference solution/spatial error test/initial.m
+++
@@ -1,5 +0,0 @@
-% This function is used in alge1D_error_space.m to represent the initial
-% value function \psi_0(x).
-
-function f = initial(x)
-f = 1./(1+x.^2).^sqrt(2);
