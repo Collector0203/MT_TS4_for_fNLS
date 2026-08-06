50c52223c3343aad649526de9dc44a922d39db25
Collector0203 <0203wxy@gmail.com>
2026-08-06 14:48:13 +0800

    Delete 1. Tests of convergence rates/3. comparison with MCF/reference solution/spatial error test directory

--- 1. Tests of convergence rates/3. comparison with MCF/reference solution/spatial error test/MT.m
+++
@@ -1,5 +0,0 @@
-% This program returns the value of the k-th MT function at point x, where
-% MT(k,x) = (1i)^k*sqrt(2/pi)*(1+2*i*x)^k/(1-2*i*x)^(k+1).
-
-function mt = MT(k,x)
-mt = (1i).^k.*sqrt(2/pi).*((1+2*1i*x)./(1-2*1i*x)).^k./(1-2*1i*x);
